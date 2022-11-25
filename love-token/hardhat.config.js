9af9f11c18d82d12e09bba07507df3fb07433e68
Abhijeet Ranjan <velocitybytecode@gmail.com>
2022-11-25 21:21:25 +0530

    fix optimizer syntax

--- love-token/hardhat.config.js
+++ love-token/hardhat.config.js
@@ -3,17 +3,19 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100000,
+      },
+    },
+  },
   networks: {
     goerli: {
       url: process.env.INFURA_GOERLI_URL,
       accounts: [process.env.PRIVATE_KEY],
-    },
-  },
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 100000,
     },
   },
 };
